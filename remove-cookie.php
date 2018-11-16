8983cdb72ff55bd69334cd8a1bbd5eeb66fef2b5
Oceanwalk10 <czlianlap@gmail.com>
2018-11-15 21:32:17 -0600

    correct step 6

--- remove-cookie.php
+++ remove-cookie.php
@@ -1,8 +1,8 @@
 <?php
 //    unset($_COOKIE['Theme']);
-    setcookie("Theme", "", -1000);
+    setcookie("Theme", "",  time() - 1);
 //    unset($_COOKIE['Philosopher']);
-    setcookie("Philosopher", "", -1000);
+    setcookie("Philosopher", "",  time() - 1);
     
     header("Location: chapter16-project1.php");
 ?>
